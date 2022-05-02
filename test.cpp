881264061c7bf4112120fc236b9ea5d6f335be14
NavigationHazard <eiselt.p@googlemail.com>
2022-05-02 15:18:54 +0200

    init repo

---
+++ test.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <string>
+#include <utility>
+
+int main() {
+    std::pair<std::string, double> pair {"Hello, World", 42};
+
+    // Structured bindings are a C++17 feature.
+    const auto& [str, num] = pair;
+    std::cout << str << ": " << num << std::endl;
+
+    return 0;
+}
